b90faf9dc4d01eb767c739edf6a369c033d9dc84
tkyyione <tkkyione@gmail.com>
2026-03-28 19:30:26 +0800

    新增套餐业务代码开发

---
+++ sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
@@ -0,0 +1,54 @@
+package com.sky.service.impl;
+
+import com.sky.dto.SetmealDTO;
+import com.sky.entity.Setmeal;
+import com.sky.entity.SetmealDish;
+import com.sky.mapper.SetmealDishMapper;
+import com.sky.mapper.SetmealMapper;
+import com.sky.service.SetmealService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class SetmealServiceImpl implements SetmealService {
+
+    private final SetmealMapper setmealMapper;
+    private final SetmealDishMapper setmealDishMapper;
+
+    public SetmealServiceImpl(SetmealMapper setmealMapper, SetmealDishMapper setmealDishMapper) {
+        this.setmealMapper = setmealMapper;
+        this.setmealDishMapper = setmealDishMapper;
+    }
+
+    /**
+     * 新增套餐
+     * @param setmealDTO
+     */
+    @Transactional
+    @Override
+    public void saveWithDish(SetmealDTO setmealDTO) {
+
+        //把DTO转换为实体类
+        Setmeal setmeal = new Setmeal();
+        BeanUtils.copyProperties(setmealDTO, setmeal);
+
+        //保存套餐主表
+        setmealMapper.insert(setmeal);
+
+        //拿到套餐id
+        Long setmealId = setmeal.getId();
+
+        //处理套餐和菜品的关系
+        List<SetmealDish> setmealDishes = setmealDTO.getSetmealDishes();
+        if(setmealDishes!=null && setmealDishes.size()>0) {
+            setmealDishes.forEach(setmealDish -> {
+                setmealDish.setSetmealId(setmealId);
+            });
+            setmealDishMapper.insertBatch(setmealDishes);
+        }
+    }
+}
